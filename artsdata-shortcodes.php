1090ded47410795c5bc700e3c2950decbf33e6ec
Gregory Saumier-Finch <gregory@culturecreates.com>
2022-11-21 09:58:16 -0500

    v1.2.1

--- artsdata-shortcodes.php
+++ artsdata-shortcodes.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: Artsdata Shortcodes
-Version: 1.2.0
+Version: 1.2.1
 Description: Collection of shortcodes to display data from Artsdata.ca.
 Author: Culture Creates
 Author URI: https://culturecreates.com/
